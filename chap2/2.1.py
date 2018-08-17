8ff907624b2810e329009bda651987f23d110b18
beterraba <thesupervisar@gmail.com>
2018-08-17 12:48:08 -0300

    2.1

--- chap2/2.1.py
+++ chap2/2.1.py
@@ -1,1 +1,20 @@
 # Splitting Strings on Any of Multiple Delimiters
+
+line = 'asdf fjdk; afed, fjek,asdf,    foo'
+import re
+re.split(r'[;,\s]\s*', line)
+
+# Discussion
+
+fields = re.split(r'(;|,|\s)\s*', line)
+fields
+
+values = fields[::2]
+values
+delimiters = fields[1::2] + ['']
+delimiters
+
+# Reform the line using the same delimiters
+''.join(v + d for v,d in zip(values, delimiters))
+
+re.split(r'(?:,|;|\s)\s*', line)
