9bc68118f928ab90d684cb9da65202b9f7d6fb63
beterraba <thesupervisar@gmail.com>
2018-08-21 12:45:54 -0300

    2.18 - 2.20

--- chap2/2.18.py
+++ chap2/2.18.py
@@ -1,2 +1,65 @@
 # Tokeninzing Text
 
+text = 'foo = 23 + 42 * 10'
+
+tokens = [('NAME','foo'), ('EQ','='),('NUM','23'),('PLUS','+'),('NUM','42'),('TIMES','*'),('NUM','10')]
+
+import re
+NAME = r'(?P<NAME>[a-zA-Z_][a-zA-Z_0-9]*)'
+NUM = r'(?P<NUM>\d+)'
+PLUS = r'(?P<PLUS>\+)'
+TIMES = r'(?P<TIMES>\*)'
+EQ = r'(?P<EQ>)\='
+WS = r'(?P<WS>\s+)'
+
+master_pat = re.compile('|'.join([NAME, NUM, PLUS, TIMES, EQ, WS]))
+
+scanner = master_pat.scanner('foo = 42')
+scanner.match()
+_.lastgroup, _.group()
+scanner.match()
+_.lastgroup, _.group()
+scanner.match()
+_.lastgroup, _.group()
+scanner.match()
+_.lastgroup, _.group()
+scanner.match()
+_.lastgroup, _.group()
+
+
+from collections import namedtuple
+
+Token = namedtuple('Token', ['type', 'value'])
+
+def generate_token(pat, text):
+    scanner = pat.scanner(text)
+    for m in iter(scanner.match, None):
+        yield Token(m.lastgroup, m.group())
+
+# Example of use
+for tok in generate_token(master_pat, 'foo = 42'):
+    print(tok)
+
+tokens = (tok for tok in generate_token(master_pat, text) if tok.type != 'WS')
+
+for tok in tokens: 
+    print(tok)
+
+
+# Discussion
+
+LT = r'(?P<LT><)'
+LE = r'(?P<LE><=)'
+EQ = r'(?P<EQ>=)'
+
+master_pat = re.compile('|'.join([LE, LT, EQ])) # Correct
+# master_pat = re.compile('|'.join([LT, LE, EQ])) # Incorrect
+
+
+PRINT = r'(?P<PRINT>print)'
+NAME = r'(?P<NAME>[a-zA-Z_][a-zA-Z_0-9]*)'
+
+master_pat = re.compile('|'.join([PRINT, NAME]))
+
+for tok in generate_token(master_pat, 'printer'):
+    print(tok)
--- chap2/2.20.py
+++ chap2/2.20.py
@@ -1,1 +1,48 @@
 # Performing Text Operations on Byte Strings
+
+data = b'Hello World'
+data[0:5]
+data.startswith(b'Hello')
+data.split()
+data.replace(b'Hello', b'Hello Cruel')
+
+data = bytearray(b'Hello World')
+data[0:5]
+data.startswith(b'Hello')
+data.split()
+data.replace(b'Hello', b'Hello cruel')
+
+data = b'FOO:BAR,SPAM'
+import re
+re.split('[:,]', data) # Error
+re.split(b'[:,]', data) # Notice: pattern as bytes
+
+
+# Discussion
+
+a = 'Hello World'
+a[0]
+a[1]
+
+b = b'Hello World'
+b[0]
+b[1]
+
+s = b'Hello World'
+print(s)
+print(s.decode('ascii'))
+
+b'%10s %10d %10.2f' % (b'ACME', 100, 490.1)
+# b'{} {} {}'.format(b'ACME', 100, 490.1) # Error
+
+'{:10s} {:10d} {:10.2f}'.format('ACME', 100, 490.1).encode('ascii')
+
+
+# Write a UTF-8 filename
+with open('jalape\xf1o.txt', 'w') as f:
+    f.write('spicy')
+
+# Get a dictionary listing
+import os
+os.listdir('.')  # Text string (names are decoded)
+os.listdir(b'.') # Byte string (names left as bytes)
