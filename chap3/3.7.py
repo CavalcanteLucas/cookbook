b79d6839a9136aba00bdefcd7e09ad16ea39750b
beterraba <thesupervisar@gmail.com>
2018-08-27 17:07:23 -0300

    3.7, 3.8

--- chap3/3.7.py
+++ chap3/3.7.py
@@ -1,1 +1,30 @@
 # Working with Infinity and NaNs
+
+a = float('inf')
+b = float('-inf')
+c = float('nan')
+[a, b, c]
+
+import math
+math.isinf(a)
+math.isnan(c)
+
+
+# Discussion
+
+a = float('inf')
+a + 45
+a * 10
+10 / a
+
+a/a
+a + b
+
+c + 23
+c / 2
+c + 2
+math.sqrt(c)
+
+d = float('nan')
+c == d
+c is d
