21ff894aa3129269764001cd68f5baa28bf10f71
beterraba <thesupervisar@gmail.com>
2018-08-29 17:43:57 -0300

    3.15, 3.16

--- chap3/3.15.py
+++ chap3/3.15.py
@@ -1,2 +1,20 @@
 # Converting Strings into Datetimes
 
+from datetime import datetime
+text = '2012-09-20'
+y = datetime.strptime(text, '%Y-%m-%d')
+z = datetime.now()
+y
+z
+diff = z - y
+diff
+
+z
+nize_z = datetime.strftime(z, '%A %B %d, %Y')
+nize_z
+
+# Faster:
+from datetime import datetime
+def parse_ymd(s):
+    year_s, mon_s, day_s = s.split('-')
+    return datetime(int(year_s), int(mon_s), int(day_s))
--- chap3/3.16.py
+++ chap3/3.16.py
@@ -1,2 +1,31 @@
 # Manipulating Dates Involving Time Zones
 
+from datetime import datetime, timedelta
+from pytz import timezone, utc
+
+d = datetime(2012, 12, 21, 9, 30, 0)
+print(d)
+
+
+# Localize the date for Chicaco
+central = timezone('US/Central')
+
+loc_d = central.localize(d)
+print(loc_d)
+
+# Convert to Bangalore time
+bang_d = loc_d.astimezone(timezone('Asia/Kolkata'))
+print(bang_d)
+
+d = datetime(2013, 3, 10, 1, 45)
+loc_d = central.localize(d)
+print(loc_d)
+later = loc_d + timedelta(minutes=30)
+print(later)
+
+
+# Discussion
+
+print(loc_d)
+utc_d = loc_d.astimezone(utc)
+print(utc_d)
