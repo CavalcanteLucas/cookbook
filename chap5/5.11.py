894313cff96fbebb043335720d20fbc9781f54b3
btrb <thesupervisar@gmail.com>
2018-11-28 12:40:27 -0300

    5.11

--- chap5/5.11.py
+++ chap5/5.11.py
@@ -1,2 +1,25 @@
 # Manipulating Pathnames
 
+import os
+path = '/User/beazley/Data/data.csv'
+
+# Get the last component of the path
+f = os.path.basename(path)
+print(f)
+
+# Get the directory name
+d = os.path.dirname(path)
+print(d)
+
+# Join path components together
+j = os.path.join('tmp', 'data', os.path.basename(path))
+print(j)
+
+# Expand the user's home directory
+path = '~/Data/data.csv'
+u = os.path.expanduser(path)
+print(u)
+
+# Split the file extensions
+s = os.path.splitext(path)
+print(s)
