4255571e3eadc6da76f7b85e005eccb60c306523
btrb <thesupervisar@gmail.com>
2019-03-14 17:09:19 -0300

    this is 5.20

--- chap5/5.20.py
+++ chap5/5.20.py
@@ -1,2 +1,12 @@
 # Communicating with Serial Ports
 
+import serial
+ser = serial.Serial('/dev/tty.usbmodem641', # Device  name varies
+                    baudrate=9600,
+                    bytesize=8,
+                    parity=N,
+                    sopbits=1)
+
+ser.write(b'G1 X50 Y50'\r\n)
+resp = ser.readline()
+
