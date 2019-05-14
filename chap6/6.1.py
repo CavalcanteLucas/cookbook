1ad6a5ce6ccf0b73274ec00e9e3ae9e29bc01401
btrb <thesupervisar@gmail.com>
2019-05-14 15:32:26 -0300

    .. returning

--- chap6/6.1.py
+++ chap6/6.1.py
@@ -1,2 +1,11 @@
 # Reading and Writing CSV Data
 
+import csv
+with open('stocks.csv') as f:
+    f_csv = csv.reader(f)
+    headers = next(f_csv)
+    for row in f_csv:
+        # Process row
+        # print(row)
+
+
